29a0f1935d9bfcb94b67c0c2aabe6aada0756f55
panoptical <neal.zupancic@gmail.com>
2014-06-03 13:24:17 +0400

    more quizzes - grade 8

---
+++ lors/quiz_score.js
@@ -0,0 +1,31 @@
+﻿var score = new Array;
+var answers=["c","b", "c","c","d","a","c","b","d","a","c","b","a","d","c"];
+
+function showScore()
+{
+  var total=0;
+  for (i in score)
+  {
+    total+=score[i];
+  }
+  document.getElementById('score').innerHTML=total;
+
+}
+
+function check(q)
+{
+ var qid="a"+q+answers[q-1];
+ var aid="answer"+q;
+ if (document.getElementById(qid).checked)
+ {
+   document.getElementById(aid).innerHTML="correct!";
+   score[q-1]=1;
+ }
+ else
+ {
+   document.getElementById(aid).innerHTML="wrong!";
+   score[q-1]=0;
+ }
+ showScore();
+
+}
---
+++ salome/questions.js
@@ -0,0 +1,29 @@
+﻿var questionArray = [
+{question:"how much is 1/4 + 2/8",
+a:"3/8",
+b:"2/4",
+c:"3/4",
+d:"1",
+answer:"2/4"
+},
+{question:"a person ate 3/10 of pizza and his friend ate 2/5. how much pizza is left?",
+a:"3/10",
+b:"5/10",
+c:"3/5",
+d:"nothing",
+answer:"3/10"
+},
+{question:"can you simplify 2/7",
+a:"7",
+b:"1/8",
+c:"no we cant",
+d:"7/7",
+answer:"no we cant"
+},
+{question:"you walked 13 kilometers which is 1/5 of your way at home. what will be 4/5 than?",
+a:"52",
+b:"30",
+c:"13",
+d:"0",
+answer:"52"
+}]
